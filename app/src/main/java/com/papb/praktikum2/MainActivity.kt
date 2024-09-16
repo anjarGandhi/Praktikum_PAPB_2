e03397c8efa4d46bfeb2092e9bd1d25a94b9acf1
anjarGandhi <gandhianjar13@gmail.com>
2024-09-16 10:21:42 +0700

    Praktikum 3

--- app/src/main/java/com/papb/praktikum2/MainActivity.kt
+++ app/src/main/java/com/papb/praktikum2/MainActivity.kt
@@ -13,8 +13,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -53,12 +59,12 @@ fun HeaderSection() {
         modifier = Modifier
             .fillMaxWidth()
             .height(56.dp)
-            .background(Color.Gray),
+            .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = "Praktikum 2",
-            color = Color.White,
+            text = "Praktikum 3",
+            color = Color.Black,
             style = MaterialTheme.typography.headlineLarge
         )
     }
@@ -68,17 +74,17 @@ fun HeaderSection() {
 fun MyScreen() {
     var text by remember { mutableStateOf("") }
     var inputText by remember { mutableStateOf("") }
+    var numberInput by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         HeaderSection()
-
 
         Row(
             modifier = Modifier
@@ -87,21 +93,72 @@ fun MyScreen() {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            TextField(
-                value = inputText,
-                onValueChange = { inputText = it },
-                label = { Text("Enter text") }
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Button(onClick = {
-                text = inputText
-            }) {
-                Text("Submit")
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Icon Profile",
+                    tint = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.size(65.dp)
+                )
+
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TextField(
+                    value = inputText,
+                    onValueChange = { inputText = it },
+                    label = { Text("Masukan Nama anda") },
+                    modifier = Modifier.weight(1f)
+                )
+
+
+
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+            TextField(
+                value = numberInput,
+                onValueChange = {
+                    if (it.length <= 15 && it.all { char -> char.isDigit() }) {
+                        numberInput = it
+                    }
+                },
+                label = { Text("Masukan NIM anda") },
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+
+
+
+
+        Button(
+            onClick = {
+                text = " $inputText, $numberInput"
+            },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Submit")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Box(
             modifier = Modifier
@@ -113,6 +170,7 @@ fun MyScreen() {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
